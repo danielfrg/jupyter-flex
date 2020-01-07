3cfc35daf6525547e00b00d74b9ef46022775cce
Daniel Rodriguez <df.rodriguez143@gmail.com>
2020-01-07 17:49:43 -0500

    Test

--- jupyter_flex/tests/conftest.py
+++ jupyter_flex/tests/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-from xprocess import ProcessStarter
 
 
 def pytest_addoption(parser):
@@ -7,23 +6,6 @@ def pytest_addoption(parser):
     group._addoption('--headless',
                      action='store_true',
                      help='enable headless mode for supported browsers.')
-
-
-@pytest.fixture(scope="module")
-def voila_server(xprocess):
-    import os
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    examples_path = os.path.join(this_dir, "..", "..", "examples")
-
-    port = "8866"
-    conn = f"http://localhost:{port}"
-
-    class Starter(ProcessStarter):
-        # pattern = ""
-        args = ["voila", "--template", "flex", "--no-browser", "--port", port, examples_path]
-
-    logfile = xprocess.ensure("voila_server", Starter)
-    return conn
 
 
 @pytest.fixture(scope="function")
