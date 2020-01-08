4f55f4725970127144e0d6655af439ba34f37eb1
Daniel Rodriguez <df.rodriguez143@gmail.com>
2020-01-07 20:40:34 -0500

    Update

--- jupyter_flex/tests/conftest.py
+++ jupyter_flex/tests/conftest.py
@@ -2,14 +2,14 @@ import pytest
 
 
 def pytest_addoption(parser):
-    group = parser.getgroup('selenium', 'selenium')
-    group._addoption('--headless',
-                     action='store_true',
-                     help='enable headless mode for supported browsers.')
+    group = parser.getgroup("selenium", "selenium")
+    group._addoption("--headless",
+                     action="store_true",
+                     help="enable headless mode for supported browsers.")
 
 
 @pytest.fixture(scope="function")
-def chrome_options(chrome_options, pytestconfig):
-    if pytestconfig.getoption('headless'):
-        chrome_options.add_argument('headless')
+def chrome_options(chrome_options, request):
+    if request.config.getoption("headless"):
+        chrome_options.add_argument("headless")
     return chrome_options
