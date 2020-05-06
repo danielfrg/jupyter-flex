bdb7baee941e802079d3aa3fa39d473d8b8608ab
Daniel Rodriguez <daniel@danielfrg.com>
2020-05-06 09:29:15 -0500

    deps

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
     author="Daniel Rodriguez",
     author_email="daniel@danielfrg.com",
     url="https://github.com/danielfrg/jupyter-flex",
-    license="Apache 2.0",
+    license="Apache License Version 2.0",
     python_requires=">=3.0,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
     install_requires=read_file("requirements.package.txt").splitlines(),
     keywords=["jupyter", "ipython", "widgets", "voila", "nbconvert", "dashboards"],
