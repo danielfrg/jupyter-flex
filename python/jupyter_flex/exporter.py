82d6c2ecbb9f6493458eafe2906772a4483cb129
Angus Hollands <goosey15@gmail.com>
2020-09-28 08:12:56 +0100

    Bugfix: restore include_external_file feature as resource

--- python/jupyter_flex/exporter.py
+++ python/jupyter_flex/exporter.py
@@ -9,6 +9,15 @@ from nbconvert.exporters.html import HTMLExporter
 from .utils import DEV_MODE
 
 
+@jinja2.contextfunction
+def include_external_file(ctx, name):
+    """Include a file relative to the notebook
+    """
+    with open(os.path.abspath(name), "r") as f:
+        content = f.read()
+    return jinja2.Markup(content)
+
+
 class FlexExporter(HTMLExporter):
 
     # "File -> Download as" menu in the notebook
@@ -22,3 +31,9 @@ class FlexExporter(HTMLExporter):
         super().__init__(*args, **kwargs)
 
         self.environment.globals["dev_mode"] = DEV_MODE
+
+    def _init_resources(self, resources):
+        resources = super()._init_resources(resources)
+
+        resources["include_external_file"] = include_external_file
+        return resources
