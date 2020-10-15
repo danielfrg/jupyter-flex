c04e2e7be5908bb50cf07bd4886b92aaaeed1ffb
Daniel Rodriguez <daniel@danielfrg.com>
2020-10-15 12:18:44 -0400

    Mobile sidebar fixes

--- js/src/Sidebar/index.js
+++ js/src/Sidebar/index.js
@@ -1,34 +1,17 @@
 import React from "react";
 
 import Card from "../Card";
-import { getTagValue } from "../utils";
+import Section from "../Section";
 
 class Sidebar extends React.Component {
     constructor(props) {
         super(props);
-        const { tags } = props;
-
-        this.state = {
-            classNames: getTagValue(tags, "class", " "),
-        };
     }
 
     render() {
-        const { cards } = this.props;
-
-        let cardComponents = [];
-        if (cards.length > 0) {
-            cards.forEach((card, i) => {
-                const cardComponent = <Card key={i} {...card} />;
-                cardComponents.push(cardComponent);
-            });
-        }
-
         return (
             <div className="sidebar col-md-2">
-                <div className="d-flex flex-column section">
-                    {cardComponents}
-                </div>
+                <Section {...this.props}></Section>
             </div>
         );
     }
