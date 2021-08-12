64e16cc59a3e2f219c8a931767015b576cd623b0
Daniel Rodriguez <daniel@danielfrg.com>
2021-08-11 20:16:03 -0500

    Real NPM release

--- js/src/Cell/widget.js
+++ js/src/Cell/widget.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { withStyles } from "@material-ui/core/styles";
-import { CircularProgress } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Typography from "@material-ui/core/Typography";
 
 import { DashboardContext } from "../App/context";
