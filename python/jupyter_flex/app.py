844bd19c1c69413f7a7f3d6f4277d40f064cfbff
Daniel Rodriguez <daniel@danielfrg.com>
2021-09-29 11:51:06 -0500

    Add base template and static paths

--- python/jupyter_flex/app.py
+++ python/jupyter_flex/app.py
@@ -13,22 +13,46 @@ def monkey_collect_template_paths(
     app_names, template_name='default', prune=False, root_dirs=None
 ):
     print("MONKEY TEMPLATES")
-    flex_templates = os.path.join(settings.templates_dir)
+
+    original = voila_paths.collect_paths(
+        app_names,
+        template_name,
+        include_root_paths=True,
+        prune=prune,
+        root_dirs=root_dirs,
+    )
+
+    flex_base_templates = os.path.join(settings.templates_dir)
     flex_voila_templates = os.path.join(
         settings.templates_dir, "voila", "flex"
     )
-    return [flex_templates, flex_voila_templates]
+    flex_templates = [flex_base_templates, flex_voila_templates]
+
+    return flex_templates + original
 
 
 def monkey_collect_static_paths(
     app_names, template_name='default', prune=False, root_dirs=None
 ):
     print("MONKEY STATIC")
+
+    original = voila_paths.collect_paths(
+        app_names,
+        template_name,
+        include_root_paths=False,
+        prune=prune,
+        root_dirs=root_dirs,
+        subdir='static',
+    )
+
     flex_static1 = os.path.join(settings.templates_dir, "nbconvert", "flex")
     flex_static2 = os.path.join(
         settings.templates_dir, "nbconvert", "flex", "static"
     )
-    return [flex_static1, flex_static2]
+
+    flex_static_templates = [flex_static1, flex_static2]
+
+    return flex_static_templates + original
 
 
 voila_paths.collect_template_paths = monkey_collect_template_paths
